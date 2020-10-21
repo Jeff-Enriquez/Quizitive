55c752aa4af8a1ccc902731e73f8771585215e87
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-10-20 23:16:44 -0700

    Update logic and style

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,38 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useStyles from './styles.js'
 
-const Home = () => {
-  const [question, setQuestion] = useState('')
-  const [answer1, setAnswer1] = useState('')
-  const [answer2, setAnswer2] = useState('')
-  const [answer3, setAnswer3] = useState('')
-  const [answer4, setAnswer4] = useState('')
+const Create = () => {
+  const [allQuestions, setAllQuestions] = useState([{
+    question: '',
+    timeLimit: '',
+    points: '',
+    answers: ['', '', '', ''],
+  }])
+  const [currentQuestion, setCurrentQuestion] = useState(0)
 
+  const [displayAllQuestions, setDisplayAllQuestions] = useState(null)
 
   const classes = useStyles()
-  
+
+  const updateQuestion = (key, value) => {
+    if(key.includes('answer')){
+      const idx = Number(key[key.length-1]) - 1
+      setAllQuestions(prevState => {
+        const newState = [...prevState]
+        newState[currentQuestion].answers[idx] = value
+        return newState
+      })
+    } else {
+      setAllQuestions(prevState => {
+        const newState = [...prevState]
+        newState[currentQuestion][key] = value
+        return newState
+      })
+    }
+    return
+  }
+
+  useEffect(() => {
+    const questions = []
+    for(let i = 0; i < allQuestions.length; i++){
+      console.log(allQuestions[i].question)
+      questions.push(
+        <div className={classes.questionContainer} key={i} onClick={() => setCurrentQuestion(i)}>
+          <p>{allQuestions[i].question.substr(0, 20)}</p>
+        </div>
+      )
+    }
+    setDisplayAllQuestions(questions)
+  }, [allQuestions])
+
   return (
     <>
       <main className={classes.mainContainer}>
-        <input type='text' placeholder='Type your question...' value={question} onChange={e => setQuestion(e.target.value)}/>
+        <input type='text' placeholder='Type your question...' value={allQuestions[currentQuestion].question} onChange={e => updateQuestion('question', e.target.value)}/>
         <div className={classes.answersContainer}>
           <div className={classes.answerContainer}>
-            <input type='text' placeholder='Add answer 1' value={answer1} onChange={e => setAnswer1(e.target.value)}/>
+            <input type='text' placeholder='Add answer 1' value={allQuestions[currentQuestion].answers[0]} onChange={e => updateQuestion('answer1', e.target.value)}/>
           </div>
           <div className={classes.answerContainer}>
-            <input type='text' placeholder='Add answer 2' value={answer2} onChange={e => setAnswer2(e.target.value)}/>
+            <input type='text' placeholder='Add answer 2' value={allQuestions[currentQuestion].answers[1]} onChange={e => updateQuestion('answer2', e.target.value)}/>
           </div>
           <div className={classes.answerContainer}>
-            <input type='text' placeholder='Add answer 3' value={answer3} onChange={e => setAnswer3(e.target.value)}/>
+            <input type='text' placeholder='Add answer 3' value={allQuestions[currentQuestion].answers[2]} onChange={e => updateQuestion('answer3', e.target.value)}/>
           </div>
           <div className={classes.answerContainer}>
-            <input type='text' placeholder='Add answer 4' value={answer4} onChange={e => setAnswer4(e.target.value)}/>
+            <input type='text' placeholder='Add answer 4' value={allQuestions[currentQuestion].answers[3]} onChange={e => updateQuestion('answer4', e.target.value)}/>
           </div>
         </div>
       </main>
-      <footer className={classes.footerContainer}></footer>
+      <footer className={classes.footerContainer}>
+        {displayAllQuestions}
+      </footer>
     </>
   )
 }
 
-export default Home
+export default Create
--- src/pages/Create/styles.js
+++ src/pages/Create/styles.js
@@ -1,25 +1,34 @@
 import { createUseStyles } from 'react-jss'
 
-const footerContainerHeight = '74px';
-
 const useStyles = createUseStyles( theme => ({
   mainContainer: {
     height: `100vh`,
     width: '100vw',
     border: '1px solid red',
     boxSizing: 'border-box',
-    marginBottom: theme.createFooterHeight,
-    marginTop: theme.headerHeight,
+    paddingBottom: theme.createFooterHeight,
+    paddingTop: theme.headerHeight,
   },
   footerContainer: {
     position: 'absolute',
     bottom: 0,
-    left: 0,
-    height: theme.createFooterHeight,
-    width: '100%',
+    left: '100vw',
     border: '1px solid black',
     boxSizing: 'border-box',
+    height: '100vw',
+    width: theme.createFooterHeight,
+    overflowY: 'auto',
+    overflowX: 'hidden',
+    transform: 'rotate(-90deg)',
+    transformOrigin: 'bottom left',
   },
+  questionContainer: {
+    transform: 'rotate(90deg)',
+    transformOrigin: 'center',
+    height: 50,
+    width: 50,
+    border: '1px solid gray',
+  }
 }))
 
 export default useStyles
